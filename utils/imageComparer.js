be22b546acda169f0ed2de5c49bbf2dc41fd86d3
Barbara Lozada <blozada@gmail.com>
2025-11-26 17:04:25 -0600

    Part 2: Spot the differences

---
+++ utils/imageComparer.js
@@ -0,0 +1,32 @@
+import resemble from 'resemblejs';
+import fs from 'fs-extra';
+import path from 'path';
+
+export async function compareImages (imagePath1, imagePath2, diffOutputPath) {
+  try {
+    if (!(await fs.pathExists(imagePath1))) {
+      throw new Error(`The image does not exist: ${imagePath1}`);
+    }
+    if (!(await fs.pathExists(imagePath2))) {
+      throw new Error(`The image does not exist: ${imagePath2}`);
+    }
+    const folder = path.dirname(diffOutputPath);
+    fs.ensureDirSync(folder);
+
+    resemble(imagePath1).
+      compareTo(imagePath2).
+      onComplete(async function (data) {
+        try {
+          console.log(data);
+          const diffBuffer = data.getBuffer();
+          await fs.writeFile(diffOutputPath, diffBuffer);
+          console.log('Diff image saved at:', diffOutputPath);
+        } catch (err) {
+          console.error('Error while saving diff image:', err.message);
+        }
+      });
+  } catch (err) {
+    console.error('Image comparison error:', err.message);
+  }
+
+}
